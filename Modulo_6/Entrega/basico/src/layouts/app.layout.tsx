831b70fa77f4e6d8fa20d4b8b2333607750424de
Tony <shidadk@hotmail.es>
2023-03-22 13:04:10 +0100

    Pequeños cambios de la base

--- Modulo_6/Entrega/basico/src/layouts/app.layout.tsx
+++ Modulo_6/Entrega/basico/src/layouts/app.layout.tsx
@@ -4,19 +4,26 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import * as classes from './app.layout.styles';
+import { useNavigate} from 'react-router-dom';
+import { linkRoutes } from 'core/router';
+
 
 interface Props {
   children: React.ReactNode;
 }
 
 export const AppLayout: React.FC<Props> = (props) => {
-  const { children } = props;
+  const { children } = props;  
+  const navigate = useNavigate();
 
+  const handleClick = () => {
+    navigate(linkRoutes.root);
+  }
   return (
     <>
       <AppBar position="static">
         <Toolbar variant="dense">
-          <IconButton color="inherit" aria-label="Menu">
+          <IconButton color="inherit" aria-label="Menu" onClick={handleClick}>
             <AccountCircle />
           </IconButton>
         </Toolbar>
